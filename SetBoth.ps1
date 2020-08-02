7ea0d213c3fc044737d79be32525cd3be3677ce8
cemvarol <cem.varol@outlook.com>
2020-08-02 20:45:16 +0100

    Create SetBoth.ps1

---
+++ SetBoth.ps1
@@ -0,0 +1,15 @@
+cd\
+mkdir cems
+
+
+
+$url1 = "raw.githubusercontent.com/cemvarol/SetResolution-Time/master/SetTime.ps1"
+$output1 = "C:\cems\SetTime.ps1"
+Invoke-WebRequest -Uri $url1 -OutFile $output1
+
+$url2 = "https://raw.githubusercontent.com/cemvarol/SetResolution-Time/master/setResolution.ps1"
+$output2 = "C:\cems\setResolution.ps1"
+Invoke-WebRequest -Uri $url2 -OutFile $output2
+
+Start-Process Powershell.exe -Argumentlist "-file C:\cems\SetTime.ps1"
+Start-Process Powershell.exe -Argumentlist "-file C:\cems\setResolution.ps1"
